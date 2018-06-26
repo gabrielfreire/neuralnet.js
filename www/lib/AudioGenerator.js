4a1bbaf1ea465ba49605245bce7e849878642e1f
Gabriel Freire <gabrielfreiredev@gmail.com>
2018-06-26 21:52:41 +0100

    delete unused code

--- www/lib/AudioGenerator.js
+++ www/lib/AudioGenerator.js
@@ -13,6 +13,8 @@ class AudioGenerator {
             this.numpyLoaded = true;
             console.log('numpy loaded');
         });
+        
+        this.wavDecoder = new WAVDecoder();
     }
     _soundFile(filePath) {
         return new Promise((resolve, reject) => {
@@ -33,31 +35,36 @@ class AudioGenerator {
           });
     }
     _spectrogram () {
-        return `def spectrogram(audioBuffer, step, wind, sampleRate):
-        w = wind
-        step = step
+        return `def spectrogram(audioBuffer, step, wind, sample_rate):
         max_freq = 8000
         eps = 1e-14
-        sample_rate = sampleRate
         samples = np.frombuffer(audioBuffer, dtype='float32')
-        assert not np.iscomplexobj(samples)
+        samples = np.nan_to_num(samples, copy=True)
+        
+        assert not np.iscomplexobj(samples), "Must not pass in complex numbers"
+        
         hop_length = int(0.001 * step * sample_rate)
-        fft_length = int(0.001 * w * sample_rate)
-        window = np.hanning(fft_length)
+        fft_length = int(0.001 * wind * sample_rate)
+        window = np.hanning(fft_length)[:, None]
         window_norm = np.sum(window ** 2)
         scale = window_norm * sample_rate
         trunc = (len(samples) - fft_length) % hop_length
         x = samples[:len(samples) - trunc]
+        
         nshape = (fft_length, (len(x) - fft_length) // hop_length + 1)
         nstrides = (x.strides[0], x.strides[0] * hop_length)
         x = as_strided(x, shape=nshape, strides=nstrides)
-        x = np.fft.rfft(x, axis=0)
+        
+        assert np.all(x[:, 1] == samples[hop_length:(hop_length + fft_length)])
+        
+        x = np.fft.rfft(x * window, axis=0)
         x = np.absolute(x)**2
         x[1:-1, :] *= (2.0 / scale)
         x[(0, -1), :] /= scale
         freqs = float(sample_rate) / fft_length * np.arange(x.shape[0])
         ind = np.where(freqs <= max_freq)[0][-1] + 1
         result = np.transpose(np.log(x[:ind, :] + eps))
+        
         return result`
     }
 
@@ -70,22 +77,22 @@ class AudioGenerator {
         const self = this;
         let spec = null;
         return new Promise((resolve, reject) => {
-            if(!this.cache[filePath]){
-                this._soundFile(filePath).then((arrayBuffer) => {
-                    this.context.decodeAudioData(arrayBuffer, (audioBuffer) => {
-                        let buffer = audioBuffer.getChannelData(0);
-                        this.cache[filePath] = buffer;
-                        if(!this.numpyLoaded) reject({ message: "Numpy was not loaded yet, try again in a few seconds" });
-                        spec = this._spectrogramFromAudioBuffer(buffer, step, wind, sampleRate);
-                        console.log(spec);
-                        resolve(spec);
-                    });
-                });
-            } else {
-                spec = this._spectrogramFromAudioBuffer(self.cache[filePath], step, wind, sampleRate);
+            this._soundFile(filePath).then((arrayBuffer) => {
+                let audioBuffer = this.wavDecoder.decode(arrayBuffer);
+                console.log(audioBuffer);
+                let buffer = audioBuffer.channelData[0];
+                this.cache[filePath] = buffer;
+                if(!this.numpyLoaded) reject({ message: "Numpy was not loaded yet, try again in a few seconds" });
+                spec = this._spectrogramFromAudioBuffer(buffer, step, wind, 16000);
                 console.log(spec);
                 resolve(spec);
-            }
+                
+            });
+            
         });
     }
+    
+
+      
 }
+
--- www/lib/wavdecoder.js
+++ www/lib/wavdecoder.js
@@ -1,98 +1,161 @@
-function WAVDecoder(data) {
-}
-
-WAVDecoder.prototype.__proto__ = Decoder.prototype;
-
-WAVDecoder.prototype.decode = function(data) {
-    var decoded = {};
-    var offset = 0;
-    // Header
-    var chunk = this.readChunkHeaderL(data, offset);
-    offset += 8;
-    if (chunk.name != 'RIFF') {
-        console.error('File is not a WAV');
-        return null;
-    }
-
-    var fileLength = chunk.length;
-    fileLength += 8;
-
-    var wave = this.readString(data, offset, 4);
-    offset += 4;
-    if (wave != 'WAVE') {
-        console.error('File is not a WAV');
-        return null;
-    }
-
-    while (offset < fileLength) {
-        var chunk = this.readChunkHeaderL(data, offset);
-        offset += 8;
-        if (chunk.name == 'fmt ') {
-            // File encoding
-            var encoding = this.readIntL(data, offset, 2);
-            offset += 2;
-
-            if (encoding != 0x0001) {
-                // Only support PCM
-                console.error('Cannot decode non-PCM encoded WAV file');
-                return null;
-            }
-
-            // Number of channels
-            var numberOfChannels = this.readIntL(data, offset, 2);
-            offset += 2;
-
-            // Sample rate
-            var sampleRate = this.readIntL(data, offset, 4);
-            offset += 4;
-
-            // Ignore bytes/sec - 4 bytes
-            offset += 4;
-
-            // Ignore block align - 2 bytes
-            offset += 2;
-
-            // Bit depth
-            var bitDepth = this.readIntL(data, offset, 2);
-            var bytesPerSample = bitDepth / 8;
-            offset += 2;
+const data_decoders = {
+    pcm8: (buffer, offset, output, channels, samples) => {
+      let input = new Uint8Array(buffer, offset);
+      let pos = 0;
+      for (let i = 0; i < samples; ++i) {
+        for (let ch = 0; ch < channels; ++ch) {
+          let data = input[pos++] - 128;
+          output[ch][i] = data < 0 ? data / 128 : data / 127;
         }
-
-        else if (chunk.name == 'data') {
-            // Data must come after fmt, so we are okay to use it's variables
-            // here
-            var length = chunk.length / (bytesPerSample * numberOfChannels);
-            var channels = [];
-            for (var i = 0; i < numberOfChannels; i++) {
-                channels.push(new Float32Array(length));
-            }
-
-            for (var i = 0; i < numberOfChannels; i++) {
-                var channel = channels[i];
-                for (var j = 0; j < length; j++) {
-                    var index = offset;
-                    index += (j * numberOfChannels + i) * bytesPerSample;
-                    // Sample
-                    var value = this.readIntL(data, index, bytesPerSample);
-                    // Scale range from 0 to 2**bitDepth -> -2**(bitDepth-1) to
-                    // 2**(bitDepth-1)
-                    var range = 1 << bitDepth - 1;
-                    if (value >= range) {
-                        value |= ~(range - 1);
-                    }
-                    // Scale range to -1 to 1
-                    channel[j] = value / range;
-                }
-            }
-            offset += chunk.length;
+      }
+    },
+    pcm16: (buffer, offset, output, channels, samples) => {
+      let input = new Int16Array(buffer, offset);
+      let pos = 0;
+      for (let i = 0; i < samples; ++i) {
+        for (let ch = 0; ch < channels; ++ch) {
+          let data = input[pos++];
+          output[ch][i] = data < 0 ? data / 32768 : data / 32767;
         }
-        else {
-            offset += chunk.length;
+      }
+    },
+    pcm24: (buffer, offset, output, channels, samples) => {
+      let input = new Uint8Array(buffer, offset);
+      let pos = 0;
+      for (let i = 0; i < samples; ++i) {
+        for (let ch = 0; ch < channels; ++ch) {
+          let x0 = input[pos++];
+          let x1 = input[pos++];
+          let x2 = input[pos++];
+          let xx = (x0 + (x1 << 8) + (x2 << 16));
+          let data = xx > 0x800000 ? xx - 0x1000000 : xx;
+          output[ch][i] = data < 0 ? data / 8388608 : data / 8388607;
         }
-    }
-    decoded.sampleRate = sampleRate;
-    decoded.bitDepth = bitDepth;
-    decoded.channels = channels;
-    decoded.length = length;
-    return decoded;
+      }
+    },
+    pcm32: (buffer, offset, output, channels, samples) => {
+      let input = new Int32Array(buffer, offset);
+      let pos = 0;
+      for (let i = 0; i < samples; ++i) {
+        for (let ch = 0; ch < channels; ++ch) {
+          let data = input[pos++];
+          output[ch][i] = data < 0 ? data / 2147483648 : data / 2147483647;
+        }
+      }
+    },
+    pcm32f: (buffer, offset, output, channels, samples) => {
+      let input = new Float32Array(buffer, offset);
+      let pos = 0;
+      for (let i = 0; i < samples; ++i) {
+        for (let ch = 0; ch < channels; ++ch)
+          output[ch][i] = input[pos++];
+      }
+    },
+    pcm64f: (buffer, offset, output, channels, samples) => {
+      let input = new Float64Array(buffer, offset);
+      let pos = 0;
+      for (let i = 0; i < samples; ++i) {
+        for (let ch = 0; ch < channels; ++ch)
+          output[ch][i] = input[pos++];
+      }
+    },
 };
+class WAVDecoder {
+    constructor(){}
+
+    decode(buffer) {
+        let pos = 0, end = 0;
+        if (buffer.buffer) {
+          // If we are handed a typed array or a buffer, then we have to consider the
+          // offset and length into the underlying array buffer.
+          pos = buffer.byteOffset;
+          end = buffer.length;
+          buffer = buffer.buffer;
+        } else {
+          // If we are handed a straight up array buffer, start at offset 0 and use
+          // the full length of the buffer.
+          pos = 0;
+          end = buffer.byteLength;
+        }
+      
+        let v = new DataView(buffer);
+      
+        function u8() {
+          let x = v.getUint8(pos);
+          pos++;
+          return x;
+        }
+      
+        function u16() {
+          let x = v.getUint16(pos, true);
+          pos += 2;
+          return x;
+        }
+      
+        function u32() {
+          let x = v.getUint32(pos, true);
+          pos += 4;
+          return x;
+        }
+      
+        function string(len) {
+          let str = '';
+          for (let i = 0; i < len; ++i)
+            str += String.fromCharCode(u8());
+          return str;
+        }
+      
+        if (string(4) !== 'RIFF')
+          throw new TypeError('Invalid WAV file');
+        u32();
+        if (string(4) !== 'WAVE')
+          throw new TypeError('Invalid WAV file');
+      
+        let fmt;
+        
+        while (pos < end) {
+          let type = string(4);
+          let size = u32();
+          let next = pos + size;
+          switch (type) {
+          case 'fmt ':
+            let formatId = u16();
+            if (formatId !== 0x0001 && formatId !== 0x0003)
+              throw new TypeError('Unsupported format in WAV file: ' + formatId.toString(16));
+            fmt = {
+              format: 'lpcm',
+              floatingPoint: formatId === 0x0003,
+              channels: u16(),
+              sampleRate: u32(),
+              byteRate: u32(),
+              blockSize: u16(),
+              bitDepth: u16(),
+            };
+            break;
+          case 'data':
+            if (!fmt)
+              throw new TypeError('Missing "fmt " chunk.');
+            let samples = Math.floor(size / fmt.blockSize);
+            let channels = fmt.channels;
+            let sampleRate = fmt.sampleRate;
+            let channelData = [];
+            for (let ch = 0; ch < channels; ++ch)
+              channelData[ch] = new Float32Array(samples);
+            this._lookup(data_decoders, fmt.bitDepth, fmt.floatingPoint)(buffer, pos, channelData, channels, samples);
+            return {
+              sampleRate: sampleRate,
+              channelData: channelData
+            };
+            break;
+          }
+          pos = next;
+        }
+      }
+      _lookup(table, bitDepth, floatingPoint) {
+        let name = 'pcm' + bitDepth + (floatingPoint ? 'f' : '');
+        let fn = table[name];
+        if (!fn)
+          throw new TypeError('Unsupported data format: ' + name);
+        return fn;
+      }
+}
